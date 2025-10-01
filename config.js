56be6e78e6cc0fb18c698ce20315f6f47771314a
wearehereagain23 <wearehereagain23@gmail.com>
2025-10-01 06:47:06 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    FIREBASE: {
+        apiKey: "AIzaSyB3HL-aufFCFMoNw1DmAC3XhX-ohS3w9Zk",
+        authDomain: "safe-bankin.firebaseapp.com",
+        projectId: "safe-bankin",
+        storageBucket: "safe-bankin.firebasestorage.app",
+        messagingSenderId: "377131759684",
+        appId: "1:377131759684:web:7ffe9836fd0b9bd240e411"
+    },
+    SUPABASE_URL: 'https://tkolyezukxoefqjzhqar.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrb2x5ZXp1a3hvZWZxanpocWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4NTU5MTMsImV4cCI6MjA2NzQzMTkxM30.S9flBLlO7p9IRA7XPZBMpc9lCC09PtveURimGeY0Nto',
+
+};
+
+
