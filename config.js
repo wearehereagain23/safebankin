0467a423f910bb5454b54391ad690160803a1327
wearehereagain23 <wearehereagain23@gmail.com>
2025-10-02 10:27:02 -0700

    rgf3rgv

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
     FIREBASE: {
-        apiKey: "AIzaSyB3HL-aufFCFMoNw1DmAC3XhX-ohS3w9Zk",
-        authDomain: "safe-bankin.firebaseapp.com",
-        projectId: "safe-bankin",
-        storageBucket: "safe-bankin.firebasestorage.app",
-        messagingSenderId: "377131759684",
-        appId: "1:377131759684:web:7ffe9836fd0b9bd240e411"
+        apiKey: "AIzaSyAn5Rd8BKfiY1XzsFPos6BgNoBqz0TnznQ",
+        authDomain: "mailalerttem.firebaseapp.com",
+        projectId: "mailalerttem",
+        storageBucket: "mailalerttem.appspot.com",
+        messagingSenderId: "1073275971845",
+        appId: "1:1073275971845:web:95386ebd36c01facfc6f6c"
     },
     SUPABASE_URL: 'https://tkolyezukxoefqjzhqar.supabase.co',
     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrb2x5ZXp1a3hvZWZxanpocWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4NTU5MTMsImV4cCI6MjA2NzQzMTkxM30.S9flBLlO7p9IRA7XPZBMpc9lCC09PtveURimGeY0Nto',
